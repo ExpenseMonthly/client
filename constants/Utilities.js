da93bf739b7230ca3a17eed3866e41fb5c212277
candra saputra <candrasaputra@live.com>
2019-10-20 15:40:13 +0700

    sementara

--- constants/Utilities.js
+++ constants/Utilities.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-const baseURL = "http://localhost:3000"
+const baseURL = "http://192.168.1.3:3000"
 
 export const UserAxios = Axios.create({
     baseURL: `${baseURL}/users`,
