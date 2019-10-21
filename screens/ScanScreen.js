b8935720d253f2300caf4745db0d0ba6560d29af
candra saputra <candrasaputra@live.com>
2019-10-21 13:50:19 +0700

    parsing data camera

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, AsyncStorage } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, AsyncStorage, ActivityIndicator } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 import { FontAwesome } from '@expo/vector-icons';
 import { TransactionAxios } from '../constants/Utilities';
-// import FormData from 'form-data';
-
 export default function ScanScreen(props) {
     const [hasCameraPermission, setHasCameraPermission] = useState('granted');
-    const [isFocused, setIsFocused] = useState(true)
+    const [isFocused, setIsFocused] = useState(true);
+    const [loading, setLoading] = useState(false);
+
     async function openCamera() {
         try {
             const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -40,35 +40,38 @@ export default function ScanScreen(props) {
             if (camera) {
                 let photo = await camera.takePictureAsync({
                     base64: true,
-                    aspect: [4, 3]
+                    aspect: [4, 3],
                 });
-
+                setLoading(true)
                 let token = await AsyncStorage.getItem("token");
 
                 let formData = new FormData();
-                formData.append("file", photo.base64);
-
+                formData.append("file", photo.uri);
+                
                 const { data } = await TransactionAxios({
                     url: '/',
                     method: "POST",
-                    data: formData,
+                    data: {
+                       photo 
+                    },
                     headers: {
-                        // Accept: 'application/json',
-                        // 'Content-Type': 'multipart/form-data',
-                        // 'Content-Type': 'application/x-www-form-urlencoded',
                         "token": token,
                     }
                 })
-
+                setLoading(false)
                 console.log(data)
             }
         } catch (error) {
+            setLoading(false)
             console.log(error)
         }
+        
     };
 
     if (!isFocused) return <View><Text>testing</Text></View>
     else if (isFocused)
+        if (loading) return <ActivityIndicator size="large" color="#E67E22" style={{ flex: 1 }} />;
+        else 
         return (
             <View style={{ flex: 1 }}>
                 <Camera style={{ flex: 1, alignItems: "center" }} type={Camera.Constants.Type.back} ref={ref => {
